5579e94ee409344819120cffbc4e9b95efa73760
Tawa Nicolas <tawanicolas@gmail.com>
2019-09-22 17:28:32 +0200

    Update drag logic

--- QuickIcon/QuickIcon/Views/TargetImageView.swift
+++ QuickIcon/QuickIcon/Views/TargetImageView.swift
@@ -9,7 +9,7 @@
 import Cocoa
 
 protocol TargetImageViewDelegate: class {
-	func received(file filePath: String, image: NSImage)
+	func received(file filePath: URL, image: NSImage)
 }
 
 class TargetImageView: NSImageView {
@@ -31,5 +31,53 @@ class TargetImageView: NSImageView {
 		layer.borderWidth = 0
 		self.layer = layer
 		self.wantsLayer = true
+		
+		registerForDraggedTypes(NSImage.imageTypes.map { NSPasteboard.PasteboardType($0) })
+	}
+	
+	override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
+		if NSImage.canInit(with: sender.draggingPasteboard),
+			sender.draggingSourceOperationMask.contains(.copy) {
+			hasBorders = true
+			
+			return .copy
+		}
+		
+		return []
+	}
+	
+	override func draggingExited(_ sender: NSDraggingInfo?) {
+		hasBorders = false
+	}
+	
+	override func prepareForDragOperation(_ sender: NSDraggingInfo) -> Bool {
+		hasBorders = false
+		
+		return NSImage.canInit(with: sender.draggingPasteboard)
+	}
+	
+	override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
+		guard !(sender.draggingSource is TargetImageView) else { return false }
+		
+		guard let image = NSImage(pasteboard: sender.draggingPasteboard) else {
+			return false
+		}
+		
+		let size = min(image.size.width, image.size.height)
+		let resizedImg = image.cropAndResize(newSize: CGSize(width: size, height: size))
+		
+		guard let resizedImage = resizedImg else {
+			return false
+		}
+		
+		self.layer?.contents = resizedImage
+		
+		if let urls = sender.draggingPasteboard.readObjects(forClasses: [NSURL.self],
+														   options: nil) as? [URL],
+			let firstUrl = urls.first {
+			delegate?.received(file: firstUrl, image: image)
+		}
+		
+		return true
 	}
 }

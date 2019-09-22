126c944ddac0399ffe3fb3316aca61b96e8bd829
Tawa Nicolas <tawanicolas@gmail.com>
2019-09-22 13:59:52 +0200

    Add new Xcode Swift Project

---
+++ QuickIcon/QuickIcon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  QuickIcon
+//
+//  Created by Tawa Nicolas on 22.09.19.
+//  Copyright © 2019 Tawa Nicolas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ QuickIcon/QuickIcon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  QuickIcon
+//
+//  Created by Tawa Nicolas on 22.09.19.
+//  Copyright © 2019 Tawa Nicolas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
